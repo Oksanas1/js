422dc437cf42f32e4989b64717db7960f676922d
Oksana <benefit989@gmail.com>
2024-05-12 00:17:44 +0300

    add clw2

---
+++ lesson13/clw2/calculator.test.js
@@ -0,0 +1,19 @@
+import calc, {getSquaredArray, getOddNumbers} from './calculator';
+
+it('Squared Array numbers', () => {
+    const sguaredNumbers = getSquaredArray([1, 2, 3]);
+
+    expect(sguaredNumbers).toEqual([1, 4, 9]);
+});
+
+it('only odd numbers', () => {
+    const oddNumbers = getOddNumbers([1, 2, 3, 4]);
+
+    expect(oddNumbers).toEqual([2, 4]);
+});
+
+it('calc', () => {
+    const addNumber = calc(2, 2);
+
+    expect(addNumber).toEqual(4);
+});
