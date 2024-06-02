27dfe1d2e1f9a129f29d966afe0e08dfd252552c
Oksana <benefit989@gmail.com>
2024-06-02 17:18:19 +0300

    fix update func

--- lesson27/hw1/src/scripts/updateTasks.js
+++ lesson27/hw1/src/scripts/updateTasks.js
@@ -1,13 +1,28 @@
 import { getLocalStorageData, setToLocalStorageData } from './storage.js';
 import { renderTasks } from './renderTasks.js';
 
-const tasks = getLocalStorageData('tasksList');
-
 export const changeStatusTasks = event => {
-  if (!event.target.classList.contains('list__item-checkbox')) return;
+  const clickedelement = event.target;
 
-  const clickedCheckBox = event.target;
-  tasks.find(task => task.id === clickedCheckBox.dataset.id).done = clickedCheckBox.checked;
-  setToLocalStorageData('tasksList', tasks);
+  if (!clickedelement.classList.contains('list__item-checkbox')) {
+    return;
+  }
+
+  const tasks = getLocalStorageData('tasksList');
+  const newTask = tasks.map(task => {
+    if (task.id === clickedelement.dataset.id) {
+      const done = clickedelement.checked;
+
+      return {
+        ...task,
+        done,
+        finishDate: done ? new Date().toISOString() : null,
+      };
+    }
+
+    return task;
+  });
+
+  setToLocalStorageData('tasksList', newTask);
   renderTasks();
 };
