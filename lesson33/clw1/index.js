a19d0a16bf0a5a86180f1293fed8bde1cef3b1f2
Oksana <benefit989@gmail.com>
2024-06-07 17:56:17 +0300

    add default img

--- lesson33/clw1/index.js
+++ lesson33/clw1/index.js
@@ -1,10 +1,11 @@
-const btnFormElem = document.querySelector('.name-form__btn');
-const nameInputElem = document.querySelector('.name-form__input');
-
 const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
-// 
+
+const btnFormElem = document.querySelector('.name-form__btn');
+const nameInputElem = document.querySelector('.name-form__input');
+
+userAvatarElem.src = 'https://img1.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D1%8D-cats-%D0%B1%D0%BE%D1%80%D1%86%D1%83%D1%85%D0%B0-8466515.jpeg'
 
 const fetchUserData = userName => {
   return fetch(`https://api.github.com/users/${userName}`)
