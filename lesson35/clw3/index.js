2e83d1f3dedda4e24e0fb002135efb326532829b
Oksana <benefit989@gmail.com>
2024-06-10 15:34:57 +0300

    first commit

---
+++ lesson35/clw3/index.js
@@ -0,0 +1,36 @@
+import { fetchRepositories, fetchUserData } from "./script/getawey.js";
+import { cleanReposList, renderRepos } from "./script/repos.js";
+import { hidenSpinner, showSpinner } from "./script/spinner.js";
+import { renderUserData } from "./script/user.js";
+
+const nameInputElem = document.querySelector('.name-form__input');
+const btnElement = document.querySelector('.name-form__btn');
+const defaultUser = {
+  name: '',
+  locatio: '',
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+};
+
+renderUserData(defaultUser);
+hidenSpinner();
+
+const onSearchUser = ()=> {
+  showSpinner();
+  cleanReposList();
+  const userName = nameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos__url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposList => {
+      renderRepos(reposList);
+    })
+    .catch(err => {
+      alert(err.message);
+    })
+    .finally(hidenSpinner());
+};
+
+btnElement.addEventListener('click', onSearchUser);
