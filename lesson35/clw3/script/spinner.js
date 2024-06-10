62bd647471a0e3c14fd5437297cd6166e17c52e0
Oksana <benefit989@gmail.com>
2024-06-10 18:27:48 +0300

    fix

--- lesson35/clw3/script/spinner.js
+++ lesson35/clw3/script/spinner.js
@@ -1,5 +1,9 @@
 const spinnerElem = document.querySelector('.spinner');
 
-export const showSpinner = () => spinnerElem.classList.remove('spinner_hidden');
+export const showSpinner = () => {
+  spinnerElem.classList.remove('spinner_hidden');
+};
 
-export const hidenSpinner = () => spinnerElem.classList.add('spinner_hidden');
+export const hidenSpinner = () => {
+  spinnerElem.classList.add('spinner_hidden');
+};
